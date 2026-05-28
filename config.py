094adb20a8a38c568ab709478d51c6c432535e32
peddakotla-sabitha <sabithareddy0000@gmail.com>
2026-05-28 22:50:33 +0530

    added environment varaibles setup

--- config.py
+++ config.py
@@ -7,3 +7,26 @@ DATABASE_URL = "postgresql+psycopg2://postgres:191036@localhost:5432/daylog_metr
 SECRET_KEY = "your_secret_key_here_change_this"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30        
+
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent
+
+DATA_FILE = BASE_DIR / "data" / "data.json"
+
+APP_NAME = "DayLog Metrics"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALGORITHM = os.getenv("ALGORITHM")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+)
